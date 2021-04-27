1ea5c38ffa795334634ed3a79f56aad41ffbc87d
Lee Greene <leebear4@gmail.com>
2021-04-27 19:34:35 -0400

    Final fixes to the verification system.

--- ban-bot.py
+++ ban-bot.py
@@ -1,13 +1,17 @@
 import discord
 
+client = discord.Client()
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
+async def on_message(message):
+    await message.author.ban()
+
+@client.event
 async def on_member_join(member):
-    await member.ban('Breaking rule 1')
-    await member.send('You have been banned for breaking rule 1.\n'
-                      '>  Do not join the server. This will result in an instant ban')
+    await member.ban()
 
 client.run('ODM0MTY4OTYwMjI4MjYxOTQx.YH8-Yg.yRIQMLqttJ_7cJ2xuDdm_za7Pjw')
