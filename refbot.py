ea1316d81201f52216b5b3ad924de96f267a5d9f
Eclipse-Darkpaw <76535332+Eclipse-Darkpaw@users.noreply.github.com>
2021-08-06 23:04:00 +0000

    Updating profile.py to not error out when someone without a profile created tries to edit their profile ID: #178891612

--- refbot.py
+++ refbot.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 prefix = '>'
 cmdlog = 'command.log'  # ???: why is this a thing? it just takes up space on the HDD. Remove to save several KB
-version_num = '1.7.1'
+version_num = '1.8.1'
 
 
 eclipse_id = 440232487738671124
@@ -148,7 +148,7 @@ async def profile(message):
         try:
             set_bio(str(message.author.id), command[2])
             await message.channel.send('Bio set')
-        except Exception:
+        except ValueError:
             await message.channel.send('Error. Bio not set, please use ASCII characters and custom emotes.')
     else:
         await display_profile(message)
