736e2857680fbd9a8ea3bfbb83bf5590ad497810
Eclipse-Darkpaw <76535332+Eclipse-Darkpaw@users.noreply.github.com>
2021-08-07 01:26:24 +0000

    misc fixes and improvements ID:

--- sunreek.py
+++ sunreek.py
@@ -16,7 +16,7 @@ start_time = time.time()
 # todo: add a master prefix only applicable to you as a back door
 
 prefix = '}'
-version_num = '1.9.1'
+version_num = '1.9.3'
 
 eclipse_id = 440232487738671124
 
@@ -32,6 +32,106 @@ unverified = 612958044132737025
 verified_role = 811522721824374834         # role to assign members who verify successfully
 questioning_role = 819238442931716137      # Role to assign when users
 mail_inbox = 840753555609878528            # modmail inbox channel
+
+counter = 0
+questions = ['Password?', 'What is your name?', 'How old are you?', 'Where did you get the link from? Please be specific. If it was a user, please use the full name and numbers(e.g. Echo#0109)', 'Why do you want to join?']
+
+
+class Application:
+    def __init__(self, applicant, channel, guild):
+        global counter
+        counter += 1
+        self.applicant = applicant
+        self.channel = channel
+        self.guild = guild
+        self.count = counter
+        self.responses = []
+
+    async def question(self):
+        global questions
+        global client
+        dm = await self.applicant.create_dm()
+        for question in questions:
+            question = '<@!' + str(self.applicant.id) + '> ' + question
+            response = await read_line(client, dm, question, self.applicant, delete_prompt=False, delete_response=False)
+            self.responses.append(response.content)
+        await dm.send('Please wait while your application is reviewed')
+
+    def gen_embed(self):
+        global questions
+
+        embed = discord.Embed(title='Application #' + str(self.count))
+        embed.set_author(name=self.applicant.name, icon_url=self.applicant.avatar_url)
+        for i in range(len(questions)):
+            embed.add_field(name=questions[i], value=self.responses[i])
+        embed.add_field(name='User ID', value=str(self.applicant.id), inline=False)
+
+        return embed
+
+    def __str__(self):
+        return 'Application for ' + str(self.applicant) + '\nWhere did you get the link from?'
+
+
+async def verify(message, verified_role):
+    guild = message.guild
+
+    if verified_role in message.guild.get_member(message.author.id).roles:
+        await message.channel.send('You are already verified')
+        return
+    try:
+        applicant = message.author
+        application = Application(applicant, message.channel, message.guild)
+        channel = guild.get_channel(application_channel)
+    except discord.errors.Forbidden:
+        message.reply('I cannot send you a message. Change your privacy settings in User Settings->Privacy & Safety')
+        return
+
+    await application.question()
+
+    applied = await channel.send(embed=application.gen_embed())
+    emojis = ['✅', '❓', '🚫', '❗']
+    for emoji in emojis:
+        await applied.add_reaction(emoji)
+
+    def check(reaction, user):
+        return user != client.user and user.guild_permissions.manage_roles and str(reaction.emoji) in emojis and reaction.message == applied
+
+    while True:
+        reaction, user = await client.wait_for('reaction_add', check=check)
+        # TODO: allow multiple mods to react at once
+        if str(reaction.emoji) == '✅':
+            await application.applicant.add_roles(guild.get_role(verified_role))
+            await message.author.send('You have been approved.')
+            await application.applicant.remove_roles(guild.get_role(questioning_role))
+            await application.applicant.remove_roles(guild.get_role(unverified))
+            await channel.send('<@!'+str(message.author.id)+'> approved')
+            break
+        elif str(reaction.emoji) == '❓':
+            await application.applicant.add_roles(guild.get_role(questioning_role))
+            await channel.send('<@!'+str(message.author.id)+'>  is being questioned')
+            await message.author.send('You have been pulled into questioning.')
+        elif str(reaction.emoji) == '🚫':
+            reason = await read_line(client, guild.get_channel(application_channel), 'Why was this user denied?', user,
+                                     delete_prompt=False, delete_response=False)
+            await message.author.send('Your application denied for:\n> ' + reason.content)
+            await channel.send('<@!'+str(message.author.id)+'> was denied for:\n> '+reason.content)
+            break
+        elif str(reaction.emoji) == '❗':
+            reason = await read_line(client, guild.get_channel(application_channel), 'Why was this user banned?', user,
+                                     delete_prompt=False, delete_response=False)
+            reason = reason.content
+            if reason == 'cancel':
+                await channel.send('Ban cancelled')
+
+            else:
+                try:
+                    await message.guild.ban(user=application.applicant,reason=reason)
+                    await channel.send('<@{}> banned for\n> {}'.format(message.author.id, reason))
+                    break
+                except discord.HTTPException:
+                    await channel.send('Ban failed. Please try again, by reacting to the message again.')
+                except discord.Forbidden:
+                    await channel.send('Error 403: Forbidden. Insufficient permissions.')
 
 
 async def ping(message):
@@ -239,8 +339,8 @@ async def on_ready():
 
 
 switcher = {'help': help, 'ping': ping, 'version_num': version, 'verify': verify, 'modmail': modmail,'quit': quit,
-            'profile': profile, 'restart': restart, 'setref': set_ref, 'ref': ref, 'kick': kick, 'ban': ban,
-            'addref': add_ref, 'crsdky': cursed_keys}
+            'profile': profile, 'restart': restart, 'setref': set_ref, 'ref': ref, 'addref': add_ref,
+            'crsdky': cursed_keys}
 
 
 @client.event
