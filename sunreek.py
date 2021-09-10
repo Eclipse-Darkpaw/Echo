9a9fd329657e56c052413aab84b01aedeaec4ddd
Eclipse-Darkpaw <76535332+Eclipse-Darkpaw@users.noreply.github.com>
2021-09-10 02:54:48 +0000

    updated version numbers ID:

--- sunreek.py
+++ sunreek.py
@@ -14,7 +14,7 @@ start_time = time.time()
 # todo: add a master prefix only applicable to you as a back door
 
 prefix = '}'
-version_num = '1.10.10'
+version_num = '1.10.11'
 
 eclipse_id = 440232487738671124
 
