b5126228fa24603af03fddc71e2db356da50b8db
Eclipse-Darkpaw <76535332+Eclipse-Darkpaw@users.noreply.github.com>
2022-01-20 17:36:45 +0000

    added base commands to anti-scam.py

--- anti-scam.py
+++ anti-scam.py
@@ -1,12 +1,38 @@
 import discord
 
-version = '1.1.0'
+version_num = '1.1.0'
 
+prefix = 'as!'
 log_channel = 933539437357432892     #channel ID of the channel where logs go
 token = 'OTMzNTQwOTg1NjY3OTkzNjcx.YejByw.dISKG7JJOBC2L3BAIPmqEpHHJMQ'          # put the bot token in the quotes
 
 game = discord.Game('Scanning for pings')
 client = discord.Client()
+
+
+async def ping(message):
+    start = time.time()
+    x = await message.channel.send('Pong!')
+    ping = time.time() - start
+    edit = x.content + ' ' + str(int(ping * 1000)) + 'ms'
+    await x.edit(content=edit)
+
+
+async def version(message):
+    await message.channel.send('I am currently running version ' + version_num)
+
+
+async def quit(message):
+    global game
+    if message.author.guild_permissions.administrator:
+        await message.channel.send('Goodbye :wave:')
+        await client.change_presence(activity=discord.Game('Going offline'))
+        sys.exit()
+    else:
+        await message.channel.send('You do not have permission to turn me off!')
+
+
+switcher = {'ping': ping, 'version': version, 'quit': quit}
 
 
 @client.event
@@ -24,8 +50,15 @@ async def on_message(message):
             embed.set_author(name='<@' + str(message.author.id) + '>', icon_url=message.author.avatar_url)
             embed.add_field(name=message, value=content)
             await channel.send(embed=embed)
-    else:
-        pass
+        if message.content.startswith(prefix):
+            command = message.content[1:].lower().split(' ', 1)
+            try:
+                method = switcher[command[0]]
+                await method(message)
+            except KeyError:
+                pass
+            if command[0] == 'print':
+                print(message.content)
 
 
 client.run(token)
