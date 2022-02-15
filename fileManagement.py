ad9a60128cacb4a8d7fede372a4d9e1bcce7d1ea
Eclipse-Darkpaw <76535332+Eclipse-Darkpaw@users.noreply.github.com>
2022-02-15 04:50:10 +0000

    Updated the ref feature of the bot

--- fileManagement.py
+++ fileManagement.py
@@ -3,31 +3,31 @@ def get_leaderboard_path(guild_id):
 
 
 def profile_path(target_id):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\profile\\{}.profile'.format(target_id)
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\profile\\{}.profile'.format(target_id)
 
 
 def ref_path(target_id):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\{}.refs'.format(target_id)
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\{}.refs'.format(target_id)
 
 
 def oc_folder_path(target_id):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\{}'.format(target_id)
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\{}'.format(target_id)
 
 
 def oc_path(target_id, oc_name):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\{}\\{}.ref'.format(target_id, oc_name)
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\{}\\{}.ref'.format(target_id, oc_name)
 
 
 def sona_path(target_id):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\' + str(target_id) + '\\00.refs'
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\' + str(target_id) + '\\00.refs'
 
 
 def char_path(target_id, char):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\' + str(target_id) + '\\' + str(char) + '.refs'
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\' + str(target_id) + '\\' + str(char) + '.refs'
 
 
 def folder_path(target_id, folder):
-    return 'C:\\Users\\leebe\\Desktop\\Bot-files\\refs\\{}\\{}'.format(target_id, folder)
+    return 'C:\\Users\\leebe\\Desktop\\Echo\\resources\\refs\\{}\\{}'.format(target_id, folder)
 
 
 def artfight_scores():
