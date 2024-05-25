5ea8430ee6947f29f7b49a57662aba51666213dd
Autumn <leebear4@gmail.com>
2024-05-25 03:35:39 -0400

    synchronization

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-start cmd /K call python bedroombot.py %1
-start cmd /K call python cyberforcebot.py %1
-start cmd /K call python Furbot.py %1
-start cmd /K call python pawbot.py %1
-start cmd /K call python sunreek.py %1
+start cmd /K call py bedroombot.py %1
+start cmd /K call py cyberforcebot.py %1
+start cmd /K call py Furbot.py %1
+start cmd /K call py pawbot.py %1
+start cmd /K call py sunreek.py %1
