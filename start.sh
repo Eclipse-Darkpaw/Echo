bf2da25fe41d00d8b5abcc7e3cf65eeda61eb750
Autumna1Equin0x <leebear4@gmail.com>
2024-05-27 18:49:47 -0400

    Added a linux start script.

---
+++ start.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- python3 bedroombot.py $1
+gnome-terminal -- python3 cyberforcebot.py
+gnome-terminal -- python3 Furbot.py $1
+gnome-terminal -- python3 pawbot.py $1
+gnome-terminal -- python3 sunreek.py $1
