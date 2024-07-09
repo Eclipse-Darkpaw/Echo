73b02d2faa26a373b4740a750131b432d3f3b62d
Autumn <autumn@autumna1equin0x.pet>
2024-07-09 02:43:17 -0400

    minor bug fixes

--- cyberforcebot.py
+++ cyberforcebot.py
@@ -42,7 +42,7 @@ async def uptime(ctx: discord.Interaction):
     :return: None
     """
     days = int(time.strftime('%j', time.gmtime(time.time() - start_time)))
-    await ctx.send(time.strftime(f'Online for {days - 1} days %H:%M:%S\n Started <t:{int(start_time)}:R>',
+    await ctx.message.send(time.strftime(f'Online for {days - 1} days %H:%M:%S\n Started <t:{int(start_time)}:R>',
                                       time.gmtime(time.time() - start_time)))
 
 
@@ -60,7 +60,7 @@ async def version(ctx: discord.Interaction):
 
 @bot.tree.command()
 async def mama(ctx):
-    await ctx.reply('Drink some water, have a snack, take your meds, and remember Mama Bruise loves you!')
+    await ctx.message.reply('Drink some water, have a snack, take your meds, and remember Mama Bruise loves you!')
 
 
 @bot.tree.command()
@@ -70,7 +70,7 @@ async def microwave(ctx):
     :param ctx:
     :return:
     """
-    await ctx.reply('You put Gemini in the microwave for 2 minutes. She comes out nice and warm when you hug '
+    await ctx.message.reply('You put Gemini in the microwave for 2 minutes. She comes out nice and warm when you hug '
                            'her')
     await ctx.channel.send('https://i.imgur.com/eOPKEV4.gif')
 
@@ -82,7 +82,7 @@ async def hug(ctx):
     :param ctx:
     :return:
     """
-    await ctx.reply('You give Gemini a hug. You can smell a faint citrus scent when you do.')
+    await ctx.message.reply('You give Gemini a hug. You can smell a faint citrus scent when you do.')
 # ADD NEW METHODS HERE!
 
 @bot.command()
