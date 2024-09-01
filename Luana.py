6babb6cbb65eeb89f66d1a2ca0f030788b8b6762
Autumn <autumn@autumna1equin0x.pet>
2024-08-31 22:47:20 -0400

    minor script updates

--- Luana.py
+++ Luana.py
@@ -110,8 +110,8 @@ async def on_message(ctx: discord.Interaction):
 
     content = ctx.content.lower()
 
-    #if not (ctx.guild is None or content.find(AntiScam.code) != -1 or ctx.channel.id in scan_ignore):
-        #await AntiScam.scan_message(ctx)
+    if not (ctx.guild is None or content.find(AntiScam.code) != -1 or ctx.channel.id in scan_ignore):
+        await AntiScam.scan_message(ctx)
 
 
 @client.event
