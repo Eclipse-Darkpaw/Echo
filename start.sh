1d9a5283a2147c4c21bcad7ec276ed735c6dd8bf
Autumn <autumn@autumna1equin0x.pet>
2024-08-31 22:13:33 -0400

    Added Luana.py and modified the start script

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ gnome-terminal -- python3 cyberforcebot.py
 gnome-terminal -- python3 Furbot.py $1
 gnome-terminal -- python3 pawbot.py $1
 gnome-terminal -- python3 sunreek.py $1
+gnome-terminal -- python3 Luana.py $1
