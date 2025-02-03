9281b521c181e0a4692ccd9e402d622e463c12fe
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-03 18:58:22 +0100

    add shebang and encapsulate arguments

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-gnome-terminal -- python3 bedroombot.py $1
+#!/bin/sh
+gnome-terminal -- python3 bedroombot.py "$1"
 gnome-terminal -- python3 cyberforcebot.py
-gnome-terminal -- python3 Furbot.py $1
-gnome-terminal -- python3 pawbot.py $1
-gnome-terminal -- python3 sunreek.py $1
-gnome-terminal -- python3 Luana.py $1
+gnome-terminal -- python3 Furbot.py "$1"
+gnome-terminal -- python3 pawbot.py "$1"
+gnome-terminal -- python3 sunreek.py "$1"
+gnome-terminal -- python3 Luana.py "$1"
