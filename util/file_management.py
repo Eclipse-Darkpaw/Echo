28665ac52238fcfebbc6de434e7c919378400344
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-06 13:30:49 +0100

    better naming

--- util/file_management.py
+++ util/file_management.py
@@ -2,22 +2,22 @@ import os
 from util import FileWatcher
 
 class FilePaths:
-    _bot_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    _data_dir = f'{_bot_path}/data'
-    _logs_dir = f'{_bot_path}/logs'
+    _root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    data_dir = f'{_root_dir}/data'
+    logs_dir = f'{_root_dir}/logs'
 
-    servers_settings = f'{_data_dir}/servers.json'
-    servers_warns = f'{_data_dir}/warns.json'
-    artfight_members = f'{_data_dir}/artfight-members.json'
-    scam_log = f'{_logs_dir}/scam.log'
+    servers_settings = f'{data_dir}/servers.json'
+    servers_warns = f'{data_dir}/warns.json'
+    artfight_members = f'{data_dir}/artfight-members.json'
+    scam_log = f'{logs_dir}/scam.log'
     
     @classmethod
     def get_ref(cls, user_id):
-        return f'{cls._data_dir}/refs/{user_id}.refs'
+        return f'{cls.data_dir}/refs/{user_id}.refs'
     
     @classmethod
     def get_nsfw_ref(cls, user_id):
-        return f'{cls._data_dir}/nsfw/refs/{user_id}.nsfwref'
+        return f'{cls.data_dir}/nsfw/refs/{user_id}.nsfwref'
 
 class WatchedFiles:
     _files = {}
