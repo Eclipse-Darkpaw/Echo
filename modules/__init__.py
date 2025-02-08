eba95e3a5383e7e00df55729fa50a5d08af6247c
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 19:58:15 +0100

    modules init

---
+++ modules/__init__.py
@@ -0,0 +1,9 @@
+from .AntiScam import AntiScam
+from .Artfight import Artfight
+from .General import General
+from .Moderation import Moderation
+from .RefManagement import RefManagement
+from .ServerSettings import ServerSettings
+from .Verification import Verification
+
+__all__ = ['AntiScam', 'Artfight', 'General', 'Moderation', 'RefManagement', 'ServerSettings', 'Verification']
