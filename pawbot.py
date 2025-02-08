27df02412bb902313bbfe0874d95b70c9ee551a5
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 21:34:20 +0100

    update pawbot to basebot & repo system

--- pawbot.py
+++ pawbot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import discord
+import logging
 import random
 import platform
 
-from discord.ext import commands
+from base_bot import EchoBot
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,33 +16,40 @@ Note: Shell config files (e.g., ~/.bashrc) are not read by load_dotenv()
 but can influence the environment before Python runs.
 """
 
-from util import setup_logger, logging
-from config import BotConfig
+from modules import (
+    AntiScam,
+    General,
+    Moderation as Mod,
+    ServerSettings as Settings,
+    Verification as Verif,
+    RefManagement as Ref
+)
 
-logger = setup_logger(log_file='logs/sunreek_info.log', console_log_level=logging.INFO, ignore_discord_logs=False)
-bot_config = BotConfig(botname='pawbot')
+from repositories import (
+    ServersSettingsRepo
+)
 
-# Modules
-import modules.AntiScam as AntiScam
-import modules.General
-import modules.Moderation as Mod
-import modules.ServerSettings as Settings
-import modules.Verification as Verif
-import modules.RefManagement as Ref
-
-#Utils
-from util.interactions import direct_message
+from util import (
+    direct_message
+)
 
 # Keep imports in alphabetical order
-
-VERSION = '4.3.0'
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix=bot_config.prefix, intents=intents)
-game = discord.Game(f'{bot_config.prefix}help for commands')
+bot = EchoBot(
+    name='pawbot',
+    version_num='4.3.0',
+    console_logging=True,
+    file_logging=True,
+    intents=intents
+)
+
+bot.add_repository(ServersSettingsRepo())
+
+game = discord.Game(f'{bot.config.prefix}help for commands')
 
 @bot.event
 async def on_ready():
@@ -52,32 +60,32 @@ async def on_ready():
     :return: None
     """
 
-    logger.info(f'We have logged in as {bot.user}')
+    bot.logger.info(f'We have logged in as {bot.user}')
 
     await bot.change_presence(activity=game)
-    if bot_config.start_notif:
+    if bot.config.start_notif:
         await direct_message(
             bot,
             f'Running, and active\n'
             '```yml\n'
-            f'{'bot_version':<15}: {VERSION}\n'
-            f'{'guardians':<15}: {', '.join(bot.get_user(int(guardian)).name for guardian in bot_config.guardians)}\n'
-            f'{'prefix':<15}: \'{bot_config.prefix}\'\n'
+            f'{'bot_version':<15}: {bot.version_num}\n'
+            f'{'guardians':<15}: {', '.join(bot.get_user(int(guardian)).name for guardian in bot.config.guardians)}\n'
+            f'{'prefix':<15}: \'{bot.config.prefix}\'\n'
             f'\n'
             f'{'system':<15}: {platform.system()}\n'
             f'{'version':<15}: {platform.version()}\n'
             f'{'python_version':<15}: {platform.python_version()}\n'
             '```',
-            *bot_config.guardians
+            *bot.config.guardians
         )
 
-    logger.info('loading cogs')
+    bot.logger.info('loading cogs')
     await bot.add_cog(Mod.Moderation(bot))
-    await bot.add_cog(modules.General.General(bot))
+    await bot.add_cog(General.General(bot))
     await bot.add_cog(Settings.Settings(bot))
     await bot.add_cog(Ref.RefManagement(bot))
     await bot.add_cog(Verif.Verification(bot))
-    logger.info('Cogs loaded')
+    bot.logger.info('Cogs loaded')
 
 
 scan_ignore = [1054172309147095130]
@@ -99,7 +107,10 @@ async def on_message(ctx: discord.Interaction):
     content = ctx.content.lower()
 
     if not (ctx.guild is None or content.find(AntiScam.BYPASS_CODE) != -1 or ctx.channel.id in scan_ignore):
-        await AntiScam.scan_message(ctx)
+        await AntiScam.scan_message(
+            ctx.message,
+            bot.repositories['servers_settings_repo'].get_guild_channel(ctx.guild.id, 'log')
+        )
 
 
 @bot.event
@@ -116,4 +127,4 @@ async def on_member_update(before, after):
                     f"remember to stop by <#1054672645527969802> for your roles.")
 
 if __name__ == '__main__':
-    bot.run(token=bot_config.token, log_handler=None)
+    bot.run(token=bot.config.token, log_handler=None)
