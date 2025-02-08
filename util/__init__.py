e7559c0748ffca976139f87c5f48b2af2d51d1b2
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 19:57:47 +0100

    update utils init

--- util/__init__.py
+++ util/__init__.py
@@ -4,5 +4,16 @@ from .file_paths import FilePaths
 from .file_modifiers import modify_json_file
 from .interactions import read_line, get_user_id, direct_message
 from .logger import setup_logger, ANSI,  logging
+from .string_operations import to_snake_case
 
-__all__ = ['DirectoryWatcher', 'FileWatcher', 'modify_json_file', 'FilePaths', 'read_line', 'get_user_id', 'direct_message', 'setup_logger']
+__all__ = [
+    'DirectoryWatcher',
+    'FileWatcher',
+    'modify_json_file',
+    'FilePaths',
+    'read_line',
+    'get_user_id',
+    'direct_message',
+    'setup_logger',
+    'to_snake_case'
+]
