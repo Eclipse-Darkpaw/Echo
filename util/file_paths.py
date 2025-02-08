6e746194cabc26d960919bec0cffca48aa59b6a1
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 17:31:07 +0100

    extract file_paths into own util

--- util/file_paths.py
+++ util/file_paths.py
@@ -1,14 +1,14 @@
 import os
-from util import FileWatcher
 
 class FilePaths:
     _root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data_dir = f'{_root_dir}/data'
     logs_dir = f'{_root_dir}/logs'
+    artfight_archive_dir = f'{data_dir}/artfight_archive'
 
     servers_settings = f'{data_dir}/servers.json'
+    artfight = f'{data_dir}/artfight.json'
     servers_warns = f'{data_dir}/warns.json'
-    artfight_members = f'{data_dir}/artfight-members.json'
     scam_log = f'{logs_dir}/scam.log'
     
     @classmethod
@@ -18,13 +18,3 @@ class FilePaths:
     @classmethod
     def get_nsfw_ref(cls, user_id):
         return f'{cls.data_dir}/nsfw/refs/{user_id}.nsfwref'
-
-class WatchedFiles:
-    _files = {}
-
-    @classmethod
-    def get_file_data(cls, file_path: str):
-        if file_path not in cls._files:
-            cls._files[file_path] = FileWatcher(file_path)
-
-        return cls._files[file_path]
