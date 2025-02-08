aab3ffd1bfc5e1c935d1b42d135ed58cbbb0e96a
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 17:32:32 +0100

    update util imports

--- util/__init__.py
+++ util/__init__.py
@@ -1,7 +1,8 @@
 from .directory_watcher import DirectoryWatcher
 from .file_watcher import FileWatcher
-from .file_management import FilePaths, WatchedFiles
+from .file_paths import FilePaths
+from .file_modifiers import modify_json_file
 from .interactions import read_line, get_user_id, direct_message
 from .logger import setup_logger, ANSI,  logging
 
-__all__ = ['DirectoryWatcher', 'FileWatcher', 'FilePaths', 'WatchedFiles', 'read_line', 'get_user_id', 'direct_message', 'setup_logger']
+__all__ = ['DirectoryWatcher', 'FileWatcher', 'modify_json_file', 'FilePaths', 'read_line', 'get_user_id', 'direct_message', 'setup_logger']
