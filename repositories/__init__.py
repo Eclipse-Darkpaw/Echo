8277b92e015feed4db037752fc8b8b85824d1c80
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-02-08 19:58:10 +0100

    repositories init

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,4 +1,10 @@
-from .servers_settings_repo import ServersSettingsRepo
+from typing import TypedDict
 from .artfight_repo import ArtfightRepo
+from .repository import Repository
+from .servers_settings_repo import ServersSettingsRepo
 
-__all__ = ['ServersSettingsRepo', 'ArtfightRepo']
+__all__ = ['ServersSettingsRepo', 'ArtfightRepo', 'Repository']
+
+class RepositoriesDict(TypedDict, total=True):
+    servers_settings_repo: ServersSettingsRepo
+    artfight_repo: ArtfightRepo
